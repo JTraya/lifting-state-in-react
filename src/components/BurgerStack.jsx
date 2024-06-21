caa12a11625091d09717d12637ac3e6846612b8c
jtraya <jtraya7@yahoo.com>
2024-06-21 01:35:01 -0500

    completed lab

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,23 @@
+// `src/components/BurgerStack.jsx`
+
+const BurgerStack = (props) => {
+    return (
+        props.stack.length > 0 ? (
+    <ul>
+        {props.stack.map((ingredient, index) => (
+            <div key={index}>
+                <li style={{backgroundColor: ingredient.color}}>
+                    {ingredient.name}
+                </li>
+                <button onClick={() => props.removeFromBurger(index)}>-</button>
+            </div>
+        ))}
+    </ul>
+        ) : (
+            <p>No Ingredients!</p>
+        )
+    )
+  };
+  
+  export default BurgerStack;
+  
